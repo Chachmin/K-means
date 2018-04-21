225fdfa91729aeecbb347c875ae073575b3d12a2
Esha <esha@iMac-Esha.local>
2018-04-21 04:29:50 +0300

    v1

---
+++ K-means_project/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace K_means_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.k_select = new System.Windows.Forms.NumericUpDown();
+            this.k_label = new System.Windows.Forms.Label();
+            this.heading = new System.Windows.Forms.Label();
+            this.subheading = new System.Windows.Forms.Label();
+            this.stats = new System.Windows.Forms.GroupBox();
+            this.label_state_2 = new System.Windows.Forms.Label();
+            this.label_state_1 = new System.Windows.Forms.Label();
+            this.label_iteration_count_2 = new System.Windows.Forms.Label();
+            this.label_iteration_count_1 = new System.Windows.Forms.Label();
+            this.label_element_count_2 = new System.Windows.Forms.Label();
+            this.label_element_count_1 = new System.Windows.Forms.Label();
+            this.button_start_anew = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.k_select)).BeginInit();
+            this.stats.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(122, 126);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(447, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Пуск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // k_select
+            // 
+            this.k_select.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.k_select.Location = new System.Drawing.Point(346, 92);
+            this.k_select.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.k_select.Name = "k_select";
+            this.k_select.Size = new System.Drawing.Size(47, 22);
+            this.k_select.TabIndex = 2;
+            this.k_select.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.k_select.ValueChanged += new System.EventHandler(this.k_select_ValueChanged);
+            // 
+            // k_label
+            // 
+            this.k_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.k_label.Location = new System.Drawing.Point(122, 92);
+            this.k_label.Name = "k_label";
+            this.k_label.Size = new System.Drawing.Size(218, 20);
+            this.k_label.TabIndex = 3;
+            this.k_label.Text = "K (количество кластеров):";
+            // 
+            // heading
+            // 
+            this.heading.Font = new System.Drawing.Font("Courier New", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.heading.Location = new System.Drawing.Point(122, 25);
+            this.heading.Name = "heading";
+            this.heading.Size = new System.Drawing.Size(400, 33);
+            this.heading.TabIndex = 4;
+            this.heading.Text = "K-means clustering";
+            this.heading.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // subheading
+            // 
+            this.subheading.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.subheading.Location = new System.Drawing.Point(122, 53);
+            this.subheading.Name = "subheading";
+            this.subheading.Size = new System.Drawing.Size(400, 23);
+            this.subheading.TabIndex = 5;
+            this.subheading.Text = "(кластеризация методом k-means)";
+            this.subheading.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // stats
+            // 
+            this.stats.Controls.Add(this.label_state_2);
+            this.stats.Controls.Add(this.label_state_1);
+            this.stats.Controls.Add(this.label_iteration_count_2);
+            this.stats.Controls.Add(this.label_iteration_count_1);
+            this.stats.Controls.Add(this.label_element_count_2);
+            this.stats.Controls.Add(this.label_element_count_1);
+            this.stats.Location = new System.Drawing.Point(122, 532);
+            this.stats.Name = "stats";
+            this.stats.Size = new System.Drawing.Size(400, 100);
+            this.stats.TabIndex = 6;
+            this.stats.TabStop = false;
+            this.stats.Text = "Информация";
+            // 
+            // label_state_2
+            // 
+            this.label_state_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_state_2.Location = new System.Drawing.Point(96, 60);
+            this.label_state_2.Name = "label_state_2";
+            this.label_state_2.Size = new System.Drawing.Size(283, 17);
+            this.label_state_2.TabIndex = 5;
+            this.label_state_2.Text = "готов к началу кластеризации (нажмите \"Пуск\")";
+            // 
+            // label_state_1
+            // 
+            this.label_state_1.AutoSize = true;
+            this.label_state_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_state_1.Location = new System.Drawing.Point(7, 60);
+            this.label_state_1.Name = "label_state_1";
+            this.label_state_1.Size = new System.Drawing.Size(83, 17);
+            this.label_state_1.TabIndex = 4;
+            this.label_state_1.Text = "Состояние:";
+            // 
+            // label_iteration_count_2
+            // 
+            this.label_iteration_count_2.AutoSize = true;
+            this.label_iteration_count_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_iteration_count_2.Location = new System.Drawing.Point(170, 41);
+            this.label_iteration_count_2.Name = "label_iteration_count_2";
+            this.label_iteration_count_2.Size = new System.Drawing.Size(16, 17);
+            this.label_iteration_count_2.TabIndex = 3;
+            this.label_iteration_count_2.Text = "0";
+            // 
+            // label_iteration_count_1
+            // 
+            this.label_iteration_count_1.AutoSize = true;
+            this.label_iteration_count_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_iteration_count_1.Location = new System.Drawing.Point(7, 40);
+            this.label_iteration_count_1.Name = "label_iteration_count_1";
+            this.label_iteration_count_1.Size = new System.Drawing.Size(154, 17);
+            this.label_iteration_count_1.TabIndex = 2;
+            this.label_iteration_count_1.Text = "Выполнено итераций:";
+            // 
+            // label_element_count_2
+            // 
+            this.label_element_count_2.AutoSize = true;
+            this.label_element_count_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_element_count_2.Location = new System.Drawing.Point(139, 20);
+            this.label_element_count_2.Name = "label_element_count_2";
+            this.label_element_count_2.Size = new System.Drawing.Size(16, 17);
+            this.label_element_count_2.TabIndex = 1;
+            this.label_element_count_2.Text = "0";
+            // 
+            // label_element_count_1
+            // 
+            this.label_element_count_1.AutoSize = true;
+            this.label_element_count_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_element_count_1.Location = new System.Drawing.Point(7, 20);
+            this.label_element_count_1.Name = "label_element_count_1";
+            this.label_element_count_1.Size = new System.Drawing.Size(132, 17);
+            this.label_element_count_1.TabIndex = 0;
+            this.label_element_count_1.Text = "Количество точек:";
+            // 
+            // button_start_anew
+            // 
+            this.button_start_anew.Location = new System.Drawing.Point(539, 552);
+            this.button_start_anew.Name = "button_start_anew";
+            this.button_start_anew.Size = new System.Drawing.Size(89, 23);
+            this.button_start_anew.TabIndex = 7;
+            this.button_start_anew.Text = "Перезапуск";
+            this.button_start_anew.UseVisualStyleBackColor = true;
+            this.button_start_anew.Click += new System.EventHandler(this.button_start_anew_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 662);
+            this.Controls.Add(this.button_start_anew);
+            this.Controls.Add(this.stats);
+            this.Controls.Add(this.subheading);
+            this.Controls.Add(this.heading);
+            this.Controls.Add(this.k_label);
+            this.Controls.Add(this.k_select);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.k_select)).EndInit();
+            this.stats.ResumeLayout(false);
+            this.stats.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown k_select;
+        private System.Windows.Forms.Label k_label;
+        private System.Windows.Forms.Label heading;
+        private System.Windows.Forms.Label subheading;
+        private System.Windows.Forms.GroupBox stats;
+        private System.Windows.Forms.Label label_element_count_2;
+        private System.Windows.Forms.Label label_element_count_1;
+        private System.Windows.Forms.Label label_iteration_count_2;
+        private System.Windows.Forms.Label label_iteration_count_1;
+        private System.Windows.Forms.Label label_state_2;
+        private System.Windows.Forms.Label label_state_1;
+        private System.Windows.Forms.Button button_start_anew;
+    }
+}
+
---
+++ K-means_project/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace K_means_project
+{
+    public partial class Form1 : Form
+    {
+        Bitmap buffer;
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;           // вид границы рабочей области
+            pictureBox1.BackColor = Color.White;                         // белый фон рабочей области
+            buffer = new Bitmap(pictureBox1.Width, pictureBox1.Height);  // экземпляр класса Bitmap размером с рабочую область
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        int shape_size      = 10;      // размер фигуры (диаметр окружности)
+        int centroid_size   = 4;       // размер фигур центроидов (сторона квадрата)
+        int k;                         // k - количество кластеров (задаётся пользователем)
+        int iteration_count = 0;       // счётчик итераций алгоритма k-means 
+
+        List<Element> Elements = new List<Element>();  // список элементов
+        List<Cluster> Clusters = new List<Cluster>();  // список кластеров
+        
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            // 1 - Получить координаты клика
+            MouseEventArgs me = (MouseEventArgs)e;
+            Elements.Add(new Element(me.Location));
+
+            // 2 - Нарисовать элемент
+            using (Graphics g = Graphics.FromImage(buffer))
+            {
+                g.DrawEllipse(Pens.Red, Elements[Elements.Count - 1].coordinates.X - shape_size/2, Elements[Elements.Count - 1].coordinates.Y - shape_size/2, shape_size, shape_size);  // поправка на размер фигуры (чтобы на месте клика был центр фигуры, а не начало)
+            }
+            pictureBox1.BackgroundImage = buffer;  // собственно рисование
+            pictureBox1.Refresh();                 // обеспечение отображения предыдущих фигур
+
+            // 3 - Активировать/деактивировать кнопку "Пуск"
+            k_check();
+
+            // 4 - Обновить инфоормацию о количестве элементов
+            label_element_count_2.Text = Convert.ToString(Elements.Count);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Enabled = false;          // исключить возможность дальнейшего нажатия на рабочую область (добавления элементов после начала анализа)
+            k_select.Enabled    = false;          // исключить возможность изменения количества кластеров
+            k = Convert.ToInt32(k_select.Value);  // зафиксировать введённое пользователем значение k
+
+            k_means();  // кластеризация
+
+            using (Graphics g = Graphics.FromImage(buffer))
+            {
+                g.Clear(pictureBox1.BackColor);
+                Pen multicolor = new Pen(Brushes.Black);
+
+                // рисуем элементы
+                for (int i = 0; i < Elements.Count; i++)
+                {
+                    multicolor.Color = Clusters[Elements[i].cluster].color;
+                    g.DrawEllipse(multicolor, Elements[i].coordinates.X - shape_size / 2, Elements[i].coordinates.Y - shape_size / 2, shape_size, shape_size);  // поправка на размер фигуры (чтобы на месте клика был центр фигуры, а не начало)
+                }
+
+                // рисуем центроиды
+                for (int j = 0; j < k; j++)
+                {
+                    multicolor.Color = Clusters[j].color;
+                    g.DrawRectangle(multicolor, Clusters[j].centroid.X - centroid_size / 2, Clusters[j].centroid.Y - centroid_size / 2, centroid_size, centroid_size);
+                }
+            }
+
+            pictureBox1.Refresh(); // собственно рисование
+            label_iteration_count_2.Text = Convert.ToString(iteration_count);  // отобразить информацию о номере итерации
+        }
+
+        public void k_means()
+        {
+            // первая итерация: задание начальных центроидов
+            if (iteration_count == 0)
+            {
+                initialize_centroids();  // инициализация центроидов    
+                assign_points();         // первоначальное отнесение точек к кластерам
+            }
+            // вторая и последующая итерации
+            else
+            {
+                if (!move_centroids())  // перемещение центроидов
+                {
+                    label_state_2.Text = "Кластеризация завершена";
+                    button1.Enabled = false;
+                    return;
+                }
+                assign_points();  // отнесение точек к кластерам (с учётом сдвинувшихся центроидов)
+            }
+            label_state_2.Text = "готов к следующей итерации (нажмите \"Пуск\")";
+            iteration_count++;
+        }
+
+        // I - инициализация центроидов
+        public void initialize_centroids()
+        {
+            Random random1 = new Random();
+
+            int optimization_attempts_remaining = 50;
+            do
+            {
+                Point random_centroid;
+                for (int j = 0; j < k; j++)
+                {
+                    do
+                    {
+                        random_centroid = Elements[random1.Next(0, Elements.Count)].coordinates;
+                    } while (Clusters.Any(cluster => cluster.centroid == random_centroid)); // проверяем, была ли уже назначена данная точка центроидом какого-либо кластера
+                    
+                    Clusters.Add(new Cluster(random_centroid, assign_colour(j)));
+                }
+            } while (centroid_optimization_required( (double)(--optimization_attempts_remaining) / 100 ));
+        }
+
+        // II - перемещение центроидов
+        public bool move_centroids()
+        {
+            bool centroids_moved = false;  // флаг смещения центроидов (для определения завершения процесса кластеризации)
+            for (int j = 0; j < k; j++)
+                {
+                    int count = 0;
+                    int x_sum = 0;
+                    int y_sum = 0;
+
+                    // ищем среднюю точку для точек, относящихся к кластеру данного центроида
+                    for (int i = 0; i < Elements.Count; i++)
+                    {
+                        if (Elements[i].cluster == j) // если данная (i-я) точка принадлежит к данному (j-му) кластеру 
+                        {
+                            x_sum += Elements[i].coordinates.X;
+                            y_sum += Elements[i].coordinates.Y;
+                            count++;
+                        }
+                    }
+                    // сдвигаем центроид в найденную точку
+                    Point new_centroid = new Point(x_sum / count, y_sum / count);
+                    if (Clusters[j].centroid != new_centroid)
+                    {
+                        Clusters[j].centroid = new_centroid;
+                        centroids_moved = true;  // зафиксировано смещение центроидов - возможно, потребуется ещё одна итерация
+                    }      
+                }
+            return centroids_moved;
+        }
+
+        // III - отнесение элементов к кластерам
+        public void assign_points()
+        {
+            for (int i = 0; i < Elements.Count; i++)
+            {
+                double min_dist = Double.MaxValue;
+                for (int j = 0; j < k; j++)
+                {
+                    int x_dist = Elements[i].coordinates.X - Clusters[j].centroid.X;
+                    int y_dist = Elements[i].coordinates.Y - Clusters[j].centroid.Y;
+                    double dist = Math.Sqrt(x_dist * x_dist + y_dist * y_dist);
+
+                    if (dist < min_dist)
+                    {
+                        min_dist = dist;
+                        Elements[i].cluster = j;
+                    }
+                }
+            }
+        }
+
+        // IV - генератор цветов
+        public Color assign_colour(int cluster_number)
+        {
+            Random colour_randomizer = new Random(Environment.TickCount);
+
+            if      (cluster_number == 0)
+                return Color.FromArgb(255, 0, 0);
+            else if (cluster_number == 1)
+                return Color.FromArgb(0, 0, 255);
+            else if (cluster_number == 2)
+                return Color.FromArgb(0, 255, 0);
+            else
+                return Color.FromArgb(colour_randomizer.Next(80, 200), colour_randomizer.Next(80, 200), colour_randomizer.Next(80, 200));
+        }
+
+        public bool centroid_optimization_required(double threshold)
+        {
+            double centroid_min_dist = Double.MaxValue;
+            for (int j = 0; j < k - 1; j++)
+            {                
+                for (int l = j + 1; l < k; l++)
+                {
+                    int x_dist = Clusters[j].centroid.X - Clusters[l].centroid.X;
+                    int y_dist = Clusters[j].centroid.Y - Clusters[l].centroid.Y;
+                    double dist = Math.Sqrt(x_dist * x_dist + y_dist * y_dist);
+
+                    if (dist < centroid_min_dist)
+                        centroid_min_dist = dist;
+                }
+            }
+
+            if (centroid_min_dist < threshold * Math.Sqrt(pictureBox1.Width * pictureBox1.Width + pictureBox1.Height * pictureBox1.Height))
+            {
+                Clusters.Clear();
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public void k_check()
+        {
+            // проверка допустимости заданного пользователем значения k
+            if (Convert.ToInt32(k_select.Value) <= Elements.Count)
+                button1.Enabled = true;
+            else
+                button1.Enabled = false;
+        }
+
+        private void k_select_ValueChanged(object sender, EventArgs e)
+        {
+            k_check();
+        }
+
+        private void button_start_anew_Click(object sender, EventArgs e)
+        {
+            Form1 NewForm = new Form1();
+            NewForm.Show();
+            this.Dispose(false);
+        }
+    }
+
+    // класс кластеризуемых элементов (точек)
+    public class Element
+    {
+        public Point coordinates { get; set; }  // координаты точки (задаются кликом)
+        public int cluster { get; set; }        // принадлежность кластеру
+
+        public Element(Point click_coordinates)
+        {
+            coordinates = click_coordinates;
+            cluster = 0;
+        }
+    }
+    public class Cluster
+    {
+        public Point centroid { get; set; }
+        public Color color { get; set; }
+
+        public Cluster(Point centroid_coordinates, Color cluster_color)
+        {
+            centroid = centroid_coordinates;
+            color = cluster_color;
+        }
+    }
+}
+
+
+
+    
+    
+    
